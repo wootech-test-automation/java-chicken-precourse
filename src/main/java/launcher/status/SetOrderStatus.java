56c787566fe2611f5ccd9fc080388f8da667de27
JaehongDev <println@kakao.com>
2022-12-11 15:30:33 +0900

    feature (SetOrderStatus) : 주문 등록 상태에 대한 기능 구현

--- src/main/java/launcher/status/SetOrderStatus.java
+++ src/main/java/launcher/status/SetOrderStatus.java
@@ -1,10 +1,16 @@
 package launcher.status;
 
 import launcher.context.OrderSystemContext;
+import view.InputView;
+import view.OutputView;
 
 public class SetOrderStatus implements OrderSystemStatus {
     @Override
     public OrderSystemStatus next(OrderSystemContext context) {
+        OutputView.printMenus(context.findAllMenu());
+        var menu = context.findMenuById(InputView.readMenu());
+        var quantity = InputView.readQuantity();
+        context.orderMenu(menu, quantity);
         return new SelectMenuStatus();
     }
 
