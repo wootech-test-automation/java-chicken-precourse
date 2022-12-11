a89725d8b2509415fc3bccdac9832e686c2314be
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-11 16:34:45 +0900

    fix: add order method

--- src/main/java/domain/repository/OrderRepository.java
+++ src/main/java/domain/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package domain.repository;
 
+import domain.type.Amount;
 import domain.type.Order;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,11 +9,33 @@ import java.util.List;
 public class OrderRepository {
     private static final List<Order> orders = new ArrayList<>();
 
-    public static List<Order> tables() {
+    public static List<Order> orders() {
         return Collections.unmodifiableList(orders);
     }
 
     public static void addOrder(Order order) {
+        if (modifyOrder(order)) {
+            return;
+        }
         orders.add(order);
+    }
+
+    private static boolean modifyOrder(Order order) {
+        for (int i = 0; i < orders.size(); ++i) {
+            Order existOrder = orders.get(i);
+            if (existOrder.equalMenu(order)) {
+                Amount newAmount = genNewAmount(order, existOrder);
+                Order newOrder = new Order(existOrder.getTable(), existOrder.getMenu(), newAmount);
+                orders.set(i, newOrder);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static Amount genNewAmount(Order order, Order existOrder) {
+        Amount existAmount = existOrder.getAmount();
+        Amount amount = order.getAmount();
+        return new Amount(existAmount.intValue() + amount.intValue());
     }
 }
