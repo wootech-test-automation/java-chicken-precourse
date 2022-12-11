73be82c659d2171e38b1c7b576e062952ffce916
JaehongDev <println@kakao.com>
2022-12-11 15:58:57 +0900

    feature (Orders) : 99건이 넘는 수량을 넘지 못하도록 검증하는 코드 작성

--- src/main/java/domain/order/Orders.java
+++ src/main/java/domain/order/Orders.java
@@ -2,16 +2,27 @@ package domain.order;
 
 import domain.category.Category;
 import domain.discount.Money;
+import exception.InvalidInputException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Orders {
-    private final static String ORDER_COLUMN_FORMAT = "메뉴 수량 금액";
+    private static final int MAX_AMOUNT = 99;
+    private static final String ORDER_COLUMN_FORMAT = "메뉴 수량 금액";
+    
     private final List<Order> orders = new ArrayList<>();
 
     public void addOrder(Order order) {
+        validateGreaterThanMaxQuantity(sumAllQuantity() + order.getQuantity());
         this.orders.add(order);
+    }
+
+    private void validateGreaterThanMaxQuantity(int amount) {
+        if (amount > MAX_AMOUNT) {
+            throw new InvalidInputException("주문 총 수량은 99개 보다 많을 수 없습니다.");
+        }
+
     }
 
     public String result() {
@@ -26,5 +37,13 @@ public class Orders {
         var beveragePrice = orders.stream().filter(order -> order.existCategory(Category.BEVERAGE))
                 .mapToInt(Order::price).sum();
         return new Money(discountChickenPrice + beveragePrice);
+    }
+
+    public boolean isRegisterOrder() {
+        return orders.size() >= 1;
+    }
+
+    private int sumAllQuantity() {
+        return orders.stream().mapToInt(Order::getQuantity).sum();
     }
 }
