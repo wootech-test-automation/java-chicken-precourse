1642199d43a65cd05390b0417e937f0845cefffd
HiiWee <wpdlzhf159@gmail.com>
2022-12-11 15:45:37 +0900

    feat: 주문에 대한 할인율 적용

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -6,11 +6,11 @@ public class Order {
     private final int quantity;
     private final int totalPrice;
 
-    public Order(final int tableNumber, final String menuName, final int quantity, final int totalPrice) {
+    public Order(final int tableNumber, final String menuName, final int quantity, final int price) {
         this.tableNumber = tableNumber;
         this.menuName = menuName;
         this.quantity = quantity;
-        this.totalPrice = totalPrice;
+        this.totalPrice = quantity * price - (quantity / 10 * 10000);
     }
 
     public int getTableNumber() {
@@ -27,5 +27,9 @@ public class Order {
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    public boolean containsTableNumber(final int tableNumber) {
+        return this.tableNumber == tableNumber;
     }
 }
