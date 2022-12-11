3870ee73b0b218d46577cd3a1d0a1dc3a54f51af
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-11 16:36:54 +0900

    feat: add view super class

---
+++ src/main/java/view/View.java
@@ -0,0 +1,25 @@
+package view;
+
+public class View {
+
+    private static final String ERROR = "[ERROR]";
+
+    protected void print(String message) {
+        System.out.println(message);
+    }
+    protected void print(Object object) {
+        System.out.println(object);
+    }
+
+    protected void printf(String format, Object... args) {
+        System.out.printf(format, args);
+    }
+
+    protected void printEmptyLine() {
+        System.out.println();
+    }
+
+    protected void printError(String message) {
+        System.out.println(ERROR  + " " + message);
+    }
+}
