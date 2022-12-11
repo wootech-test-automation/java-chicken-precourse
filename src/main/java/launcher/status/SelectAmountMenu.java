e293038c3f3061f611e67d8eed88f33ce1be7849
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-11 16:38:36 +0900

    feat: add select amount status

---
+++ src/main/java/launcher/status/SelectAmountMenu.java
@@ -0,0 +1,27 @@
+package launcher.status;
+
+import domain.type.Amount;
+import domain.type.Menu;
+import domain.type.Order;
+import domain.type.Table;
+import launcher.ChickenLauncher;
+
+public class SelectAmountMenu extends AbstractChickenGameStatus {
+
+    private final Table table;
+    private final Menu menu;
+
+    public SelectAmountMenu(ChickenLauncher context, Table table, Menu menu) {
+        super(context);
+        this.table = table;
+        this.menu = menu;
+    }
+
+    @Override
+    public ChickenGameStatus process() {
+        Amount amount = inputView.inputAmount();
+        Order order = new Order(table, menu, amount);
+        chickenGame.saveOrder(order);
+        return new MainMenuGameStatus(context);
+    }
+}
