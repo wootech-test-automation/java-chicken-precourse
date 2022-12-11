b80f2ef6c06c892addfea6ccaa3201959b312e43
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-11 16:36:31 +0900

    feat: add input view

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,65 @@
 package view;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+import domain.type.Amount;
+import domain.type.PaymentType;
+import domain.type.Table;
+import exception.IllegalInputArgumentException;
+import java.util.IllegalFormatException;
+import launcher.keyword.MainFeatureKeyword;
 
-public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+public class InputView extends View {
 
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+    private static final String NUMBER_FORMAT_MESSAGE = "숫자 형태로 입력해주세요.";
+    public static final String REQUEST_MAIN_FEATURE = "## 메인화면\n"
+            + "1 - 주문등록\n"
+            + "2 - 결제하기\n"
+            + "3 - 프로그램 종료\n"
+            + "\n"
+            + "## 원하는 기능을 선택하세요.";
+
+    private static final String REQUEST_TABLE_NUMBER = "## 테이블을 선택하세요.";
+    private static final String REQUEST_MENU_NUMBER = "## 등록할 메뉴를 선택하세요.";
+    private static final String REQUEST_AMOUNT_NUMBER = "## 메뉴의 수량을 입력하세요.";
+
+    public MainFeatureKeyword inputMainFeature() {
+        print(REQUEST_MAIN_FEATURE);
+        String line = Console.readLine();
+        return MainFeatureKeyword.keywordOf(line);
+    }
+
+
+    public Table inputTableNumber() {
+        try {
+            print(REQUEST_TABLE_NUMBER);
+            int number = Integer.parseInt(Console.readLine());
+            return new Table(number);
+        } catch (IllegalFormatException e) {
+            throw new IllegalInputArgumentException(NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    public int inputMenu() {
+        try {
+            print(REQUEST_MENU_NUMBER);
+            return Integer.parseInt(Console.readLine());
+        } catch (IllegalFormatException e) {
+            throw new IllegalInputArgumentException(NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    public Amount inputAmount() {
+        try {
+            print(REQUEST_AMOUNT_NUMBER);
+            int number = Integer.parseInt(Console.readLine());
+            return new Amount(number);
+        } catch (IllegalFormatException e) {
+            throw new IllegalInputArgumentException(NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    public PaymentType inputPay() {
+        String line = Console.readLine();
+        return PaymentType.keywordOf(line);
     }
 }
