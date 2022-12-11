df81a05a18ed6dd742869d216573528e979f415a
HiiWee <wpdlzhf159@gmail.com>
2022-12-11 16:34:54 +0900

    feat: 주문 수량이 범위를 벗어나면 예외 발생 메시지 추가

--- src/main/java/message/ErrorMessage.java
+++ src/main/java/message/ErrorMessage.java
@@ -5,6 +5,7 @@ public class ErrorMessage {
     public static final String INVALID_NUMERIC_ERROR = "[ERROR] 숫자만 입력해야 합니다.";
     public static final String OUT_BOUNDS_TABLE_NUMBER = "[ERROR] 존재하는 테이블 번호를 입력해야 합니다.";
     public static final String NO_MENU_ERROR = "[ERROR] 존재하지 않는 메뉴 입니다.";
+    public static final String OUT_BOUNDS_QUANTITY = "[ERROR] 주문 가능한 수량은 1 ~ 99개 입니다.";
 
     private ErrorMessage() {
     }
