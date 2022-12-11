5ac7fea961ace0970d6115c091d5012b487cef4f
JaehongDev <println@kakao.com>
2022-12-11 15:26:51 +0900

    feature (Order) : 주문에 대한 기능 구현

---
+++ src/main/java/domain/order/Order.java
@@ -0,0 +1,28 @@
+package domain.order;
+
+import domain.category.Category;
+import domain.menu.Menu;
+import domain.quantity.Quantity;
+
+public class Order {
+    private static final String ORDER_RESULT_FORMAT = "%s %s %s";
+    private final Quantity quantity;
+    private final Menu menu;
+
+    public Order(Quantity quantity, Menu menu) {
+        this.quantity = quantity;
+        this.menu = menu;
+    }
+
+    public String result() {
+        return String.format(ORDER_RESULT_FORMAT, menu.getName(), quantity.number(), menu.price());
+    }
+
+    public int price() {
+        return menu.price() * quantity.number();
+    }
+
+    public boolean existCategory(Category category) {
+        return menu.sameCategory(category);
+    }
+}
