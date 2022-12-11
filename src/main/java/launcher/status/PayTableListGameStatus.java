13c69ae5b817baa98ade36df9f70beb32075ebdf
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-11 17:55:44 +0900

    feat: add validation when choose not ordered table

--- src/main/java/launcher/status/PayTableListGameStatus.java
+++ src/main/java/launcher/status/PayTableListGameStatus.java
@@ -7,7 +7,8 @@ import launcher.ChickenLauncher;
 
 public class PayTableListGameStatus extends AbstractChickenGameStatus {
 
-    private static final String ERROR_MESSAGE = "테이블에 없는 값을 입력했습니다.";
+    private static final String ERROR_EMPTY_TABLE = "테이블에 없는 값을 입력했습니다.";
+    private static final String ERROR_CANNOT_PAY_TABLE = "빈 테이블을 결제할 수 없습니다. 처음으로 돌아갑니다.";
 
     public PayTableListGameStatus(ChickenLauncher context) {
         super(context);
@@ -19,12 +20,25 @@ public class PayTableListGameStatus extends AbstractChickenGameStatus {
         outputView.printTables(tables);
         Table table = inputView.inputTableNumber();
         validateTable(tables, table);
+        if (isNotOrderedTable(tables, table)) {
+            return new MainMenuGameStatus(context);
+        }
         return new PayGameGameStatus(context, table);
     }
 
     private void validateTable(List<Table> tables, Table table) {
         if (!tables.contains(table)) {
-            throw new IllegalInputArgumentException(ERROR_MESSAGE);
+            throw new IllegalInputArgumentException(ERROR_EMPTY_TABLE);
         }
+    }
+
+    private boolean isNotOrderedTable(List<Table> tables, Table table) {
+        for (Table existTable : tables) {
+            if (existTable.equals(table) && !existTable.isOrdered()) {
+                outputView.printError(ERROR_CANNOT_PAY_TABLE);
+                return true;
+            }
+        }
+        return false;
     }
 }
