f386542309e65101de5a63d2df30957053f7028f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-11 17:24:41 +0900

    feat: add set order table method

--- src/main/java/domain/repository/OrderRepository.java
+++ src/main/java/domain/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package domain.repository;
 
 import domain.type.Amount;
+import domain.type.Menu;
 import domain.type.Order;
+import domain.type.Table;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderRepository {
     private static final List<Order> orders = new ArrayList<>();
@@ -18,6 +21,28 @@ public class OrderRepository {
             return;
         }
         orders.add(order);
+    }
+
+    public void setOrderTable(Table table) {
+        for (int i = 0; i < orders.size(); ++i) {
+            Table existTable = orders.get(i).getTable();
+            Menu existMenu = orders.get(i).getMenu();
+            Amount existAmount = orders.get(i).getAmount();
+            if (existTable.getNumber() == table.getNumber()) {
+                orders.set(i, new Order(table, existMenu, existAmount));
+            }
+        }
+    }
+
+    public static List<Order> getOrders(Table table) {
+        return OrderRepository.orders().stream()
+                .filter(order -> order.getTable().equals(table))
+                .collect(Collectors.toList());
+    }
+
+    public static void removeAll(Table table) {
+        List<Order> tableOrders = OrderRepository.getOrders(table);
+        orders.removeAll(tableOrders);
     }
 
     private static boolean modifyOrder(Order order) {
