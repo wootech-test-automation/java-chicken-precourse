474a1e288bf36648d5d63d4847ae2e52e5082eb7
JaehongDev <println@kakao.com>
2022-12-11 15:30:16 +0900

    feature (PaymentStatus) : 결제 상태에 대한 기능 구현

--- src/main/java/launcher/status/PaymentStatus.java
+++ src/main/java/launcher/status/PaymentStatus.java
@@ -1,15 +1,22 @@
 package launcher.status;
 
 import launcher.context.OrderSystemContext;
+import view.InputView;
+import view.OutputView;
 
 public class PaymentStatus implements OrderSystemStatus {
     @Override
     public OrderSystemStatus next(OrderSystemContext context) {
+        OutputView.printOrderList();
+        var price = context.findOrderByTable()
+                .calculateAll()
+                .discount(InputView.readPayments());
+        OutputView.printFinalPaymentAmount(price);
         return new SelectMenuStatus();
     }
 
     @Override
     public boolean runnable() {
-        return false;
+        return true;
     }
 }
